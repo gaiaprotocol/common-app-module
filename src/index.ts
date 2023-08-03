6ac08d49557f00a332d249e18c5afc0184d4419d
Sim Young Jae <hanul@hanul.me>
2023-08-03 13:41:43 +0900

    Add Router to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export { default as BodyNode } from "./dom/BodyNode.js";
 export { default as DomNode } from "./dom/DomNode.js";
 export { default as ResponsiveImage } from "./dom/ResponsiveImage.js";
 export { default as el } from "./dom/el.js";
+export { default as Router } from "./view/Router.js";
 export { default as View, ViewParams } from "./view/View.js";
 
