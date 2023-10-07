4d78b3cf3acb9476e3d531f6c51772e47e701e7b
Sim Young Jae <hanul@hanul.me>
2023-10-07 14:05:59 +0900

    Fix class name issue

--- src/component/MaterialIcon.ts
+++ src/component/MaterialIcon.ts
@@ -2,7 +2,7 @@ import Component from "./Component.js";
 
 export default class MaterialIcon extends Component {
   constructor(iconName: string) {
-    super("span.icon.material-symbols-outlined");
+    super("span.material-icon.material-symbols-outlined");
     this.text = iconName;
   }
 }
