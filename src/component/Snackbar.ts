35760d928820cb3dd224ba4392e02931b180922c
Sim Young Jae <hanul@hanul.me>
2023-10-09 15:34:57 +0900

    Update snackbar component

--- src/component/Snackbar.ts
+++ src/component/Snackbar.ts
@@ -1,7 +1,9 @@
+import BodyNode from "../dom/BodyNode.js";
 import el from "../dom/el.js";
 import Component from "./Component.js";
 import MaterialIcon from "./MaterialIcon.js";
 import Button from "./button/Button.js";
+import ButtonType from "./button/ButtonType.js";
 
 export default class Snackbar extends Component {
   constructor(options: {
@@ -14,21 +16,27 @@ export default class Snackbar extends Component {
     super(".snackbar");
 
     this.append(
-      el("p", options.message),
-      options.action
-        ? new Button({
-          title: options.action.title,
-          click: () => {
-            options.action?.click();
-            this.delete();
-          },
-        })
-        : undefined,
-      el("button", new MaterialIcon("close"), {
-        click: () => this.delete(),
-      }),
+      el(
+        "main",
+        el("p", options.message),
+        options.action
+          ? new Button({
+            tag: ".action",
+            type: ButtonType.Text,
+            title: options.action.title,
+            click: () => {
+              options.action?.click();
+              this.delete();
+            },
+          })
+          : undefined,
+        el("button.close", new MaterialIcon("close"), {
+          click: () => this.delete(),
+        }),
+      ),
     );
+    BodyNode.append(this);
 
-    setTimeout(() => this.delete(), 2750);
+    setTimeout(() => this.delete(), 5000);
   }
 }
