976871d474d457467fd8b3f57d7f17a54e19f6d3
yjgaia <hanul@hanul.me>
2024-04-02 21:46:46 +0900

    dev

--- src/SplashLoader.ts
+++ src/SplashLoader.ts
@@ -1,10 +1,19 @@
 import SplashScreen from "./component/SplashScreen.js";
+import ErrorAlert from "./component/dialogue/ErrorAlert.js";
 import { DomChild } from "./dom/DomNode.js";
 
 class SplashLoader {
   public async load(logo: DomChild, promises: Promise<any>[]) {
     const splashScreen = new SplashScreen(logo);
-    await Promise.all(promises);
+    try {
+      await Promise.all(promises);
+    } catch (e: any) {
+      new ErrorAlert({
+        title: "Error",
+        message: e.message,
+      });
+      throw e;
+    }
     splashScreen.delete();
   }
 }
