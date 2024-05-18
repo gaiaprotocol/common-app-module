cef7dda8e2fb0961497f1d6ea3046f9dc3df2249
yjgaia <yjdev88@gmail.com>
2024-05-18 14:40:42 +0900

    build

--- lib/component/LottieAnimation.d.ts
+++
@@ -1,7 +0,0 @@
-import Component from "./Component.js";
-export default class LottieAnimation extends Component {
-    private animation;
-    constructor(tag: string, animationData: any);
-    delete(): void;
-}
-//# sourceMappingURL=LottieAnimation.d.ts.map
--- lib/view/ExitAppPopup.d.ts
+++
@@ -1,5 +0,0 @@
-import Popup from "../component/exitable/Popup.js";
-export default class ExitAndroidAppPopup extends Popup {
-    constructor();
-}
-//# sourceMappingURL=ExitAppPopup.d.ts.map
